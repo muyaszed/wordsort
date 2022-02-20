bac586264c1f2e97826b1e992ac193852ccc2874
Yazed Jamal <yazedjamal@Yazeds-Air.lan>
2022-02-20 07:06:39 +0000

    fixed board alignment

--- lib/wordList.dart
+++ lib/wordList.dart
@@ -4,41 +4,36 @@ class WordList extends StatelessWidget {
   const WordList(
       {Key? key,
       required this.solutionCheck,
-      required this.showList,
       required this.wordList,
       this.containerWidth})
       : super(key: key);
   final List<String> solutionCheck;
-  final bool showList;
   final List<String> wordList;
   final double? containerWidth;
 
   @override
   Widget build(BuildContext context) {
-    return showList
-        ? Wrap(
-            spacing: 20,
-            direction: Axis.vertical,
-            children: wordList.map((e) {
-              TextStyle style;
+    return Wrap(
+      spacing: 20,
+      direction: Axis.vertical,
+      children: wordList.map((e) {
+        TextStyle style;
 
-              if (solutionCheck.contains(e)) {
-                style = const TextStyle(color: Color(0xffffffff));
-              } else {
-                style =
-                    const TextStyle(color: Color.fromARGB(255, 146, 212, 120));
-              }
+        if (solutionCheck.contains(e)) {
+          style = const TextStyle(color: Color(0xffffffff));
+        } else {
+          style = const TextStyle(color: Color.fromARGB(255, 146, 212, 120));
+        }
 
-              return Container(
-                alignment: Alignment.center,
-                width: containerWidth ?? 150,
-                child: Text(
-                  e.toUpperCase(),
-                  style: style,
-                ),
-              );
-            }).toList(),
-          )
-        : const SizedBox();
+        return Container(
+          alignment: Alignment.center,
+          width: containerWidth ?? 150,
+          child: Text(
+            e.toUpperCase(),
+            style: style,
+          ),
+        );
+      }).toList(),
+    );
   }
 }
